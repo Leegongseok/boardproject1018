b2eec0632dc30ccfe8b705e93fd8c7f86a21e18b
Leegongseok <seok20108014@gmail.com>
2024-11-01 15:48:52 +0900

    중간고사과제

---
+++ board-main/src/main/java/com/codingrecipe/board/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.codingrecipe.board.controller;
+
+import com.codingrecipe.board.domain.User;
+import com.codingrecipe.board.dto.UserDTO;
+import com.codingrecipe.board.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO) {
+        User newUser = userService.createUser(userDTO);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUser(@PathVariable String username) {
+        Optional<User> user = userService.findUserByUsername(username);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
---
+++ board-main/src/main/java/com/codingrecipe/board/service/UserService.java
@@ -0,0 +1,27 @@
+package com.codingrecipe.board.service;
+
+import com.codingrecipe.board.domain.User;
+import com.codingrecipe.board.dto.UserDTO;
+import com.codingrecipe.board.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserDTO userDTO) {
+        User user = new User();
+        user.setUsername(userDTO.getUsername());
+        user.setPassword(userDTO.getPassword());
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
